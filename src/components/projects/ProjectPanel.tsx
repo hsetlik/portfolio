cf52a83a4c3b066d7d76c6b85c4126b9dcaf02d0
Hayden Setlik <haydensetlik@gmail.com>
2022-06-21 13:12:21 -0600

    switched to img element

--- src/components/projects/ProjectPanel.tsx
+++ src/components/projects/ProjectPanel.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { ProjectInfo } from "../../app/projectInfo";
 export default function ProjectPanel({name, desc, image}: ProjectInfo) {
-    let _className = 'project-panel' + (image) ? ' has-bg-img' : '';
-    let _style: React.CSSProperties = {};
-    if (image) {
-        console.log('Image at: ' + image);
-        _style = {backgroundImage: `url(${image})`};
-    }
+    let _className = 'project-panel' + ((image) ? ' has-bg-img' : '');
     return (
-        <div className={_className} style={_style}>
+        <div className={_className}>
             <h3>{name}</h3>
             <p>{desc}</p>
+
+            {image && (
+                <img src={image} className='bg-img img-fluid' />
+            )}
         </div>
     )
 }
