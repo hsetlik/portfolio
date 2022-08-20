c5c92fbeea80ff0819e06ddc21f05136df1e0f0f
Hayden Setlik <haydensetlik@gmail.com>
2022-08-20 15:32:59 -0600

    contact form styles

--- src/components/contact/ContactRoute.tsx
+++ src/components/contact/ContactRoute.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import EmailForm from "./EmailForm";
 // import { useLocation } from "react-router-dom";
+import "../../styles/contact.css";
 
-export default function ContactRoute(){
-    /* 
-        this needs:
-        1. Header
-        2. Email link
-        3. Profile links (github/linkedin
-    */
-//    const loc = useLocation();
-//    console.log(`Contact route initialized at: ${loc.pathname}`);
+export default function ContactRoute() {
+    // TODO: add links to github/linkedin profiles
     return (
         <div className="container align-self-center bg-primary">
             <div className="col">
-                <h2>Contact me</h2>
                 <div className="row">
-                    <EmailForm />
-                </div> 
-                
+                    <div className="col">
+                        <h2>Contact me:</h2>
+                    </div>
+                    <div className="col bg-dark">
+                        <label>Find me on:</label>
+                        <div className="row">
+                            <span>GitHub</span>
+                            <img className='lang-logo' alt='gh' src='img/tool-logos/github_logo.svg'></img>
+                        </div>
+                    </div>
+                </div>
+                <p>
+                    You can get in touch with me via email in the form below or directly at haydensetlik@gmail.com.
+                </p>
+                <EmailForm />
             </div>
         </div>
     )
