19717803ac60fc7178b018324e9dbc3d7fc3b5d9
Hayden Setlik <haydensetlik@gmail.com>
2022-08-29 07:33:37 -0600

    mouseover info for framework labels

--- src/app/skillsInfo.ts
+++ src/app/skillsInfo.ts
@@ -10,69 +10,90 @@ export const languages = [
 export interface Framework {
     name: string,
     language: string,
-    desc?: string,
+    desc: string,
     experience?: number,
-    image?: string,
-    usedIn?: string[], // store names of relevant projects here
+    image: string,
+    infoLink: string
 }
 
 export const frameworks: Framework[] = [
    {
     name: 'JUCE Framework',
     language: 'C++',
-    image: '/portfolio/img/framework-logos/juce_logo.svg'
+    image: '/portfolio/img/framework-logos/juce_logo.svg',
+    desc: 'C++ framework for audio plugins and standalone audio applications',
+    infoLink: 'https://juce.com/'
    },
    {
     name: 'React.js',
     language: 'JavaScript/TypeScript',
-    image: '/portfolio/img/framework-logos/react_logo.svg'
+    image: '/portfolio/img/framework-logos/react_logo.svg',
+    desc: 'The world\'s most popular JavaScript framework',
+    infoLink: 'https://reactjs.org/'
    },
    {
-    name: 'ASP.NET',
+    name: 'ASP.NET 6.0',
     language: 'C#',
-    image: '/portfolio/img/framework-logos/dotnet_logo.svg'
+    image: '/portfolio/img/framework-logos/dotnet_logo.svg',
+    desc: 'Cross-platform framework for building applications with C#',
+    infoLink: 'https://dotnet.microsoft.com/en-us/apps/aspnet'
    },
    {
     name: 'Arduino framework',
     language: 'C++',
-    image: '/portfolio/img/framework-logos/arduino_logo.svg'
+    image: '/portfolio/img/framework-logos/arduino_logo.svg',
+    desc: 'Popular C++ framework for microcontroller firmware development',
+    infoLink: 'https://docs.platformio.org/en/stable/frameworks/arduino.html'
    }
 ]
 
 export interface Tool {
     name: string,
     desc?: string,
-    image?: string
+    image?: string,
+    infoLink?: string
 }
 
 export const tools: Tool[] = [
     {
         name: 'Git/Github',
-        image: '/portfolio/img/tool-logos/github_logo.svg'
+        image: '/portfolio/img/tool-logos/github_logo.svg',
+        desc: 'Industry standard version control software',
+        infoLink: 'https://git-scm.com/'
     },
     {
         name: 'Azure App Service',
-        image: '/portfolio/img/tool-logos/azure_logo.svg'
+        desc: 'Cloud application service',
+        image: '/portfolio/img/tool-logos/azure_logo.svg',
+        infoLink: 'https://azure.microsoft.com/en-us/services/app-service/'
     },
     {
         name: 'PlatformIO',
-        //desc: 'IDE and toolkit for all things embedded C++',
-        image: '/portfolio/img/tool-logos/platformio_logo.svg'
+        desc: 'IDE and toolkit for all things embedded C++',
+        image: '/portfolio/img/tool-logos/platformio_logo.svg',
+        infoLink: 'https://platformio.org/'
     },
     {
         name: 'node.js / npm',
-        image: '/portfolio/img/tool-logos/npm_logo.svg'
+        desc: 'Ubiquitous JavaScript runtime',
+        image: '/portfolio/img/tool-logos/npm_logo.svg',
+        infoLink: 'https://nodejs.org/en/'
     },
     {
-        name: 'PostGreSQL',
-        image: '/portfolio/img/tool-logos/postgresql_logo.svg'
+        name: 'PostgreSQL',
+        desc: 'Open-source database software',
+        image: '/portfolio/img/tool-logos/postgresql_logo.svg',
+        infoLink: 'https://www.postgresql.org/'
     },
     {
         name: 'Bootstrap 5',
-        image: '/portfolio/img/tool-logos/bootstrap_logo.svg'
+        desc: 'Popular CSS framework',
+        image: '/portfolio/img/tool-logos/bootstrap_logo.svg',
+        infoLink: 'https://getbootstrap.com/docs/5.0/getting-started/introduction/'
     },
     {
         name: 'Linux / shell scripting',
-        image: '/portfolio/img/tool-logos/linux_logo.svg'
+        image: '/portfolio/img/tool-logos/linux_logo.svg',
+        infoLink: 'https://www.linux.org/'
     }
 ]
--- src/components/skills/FrameworkLabel.tsx
+++ src/components/skills/FrameworkLabel.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Framework } from '../../app/skillsInfo'
 
 interface Props {
     framework: Framework;
 }
-
 export default function FrameworkLabel({ framework }: Props) {
+    const [hovering, setHovering] = useState(false);
+    const onOver = () => setHovering(true);
+    const onExit = () => setHovering(false);
     return (
-            <div className='col frm-label bg-dark text-light'>
+        <div onMouseOver={onOver} onMouseLeave={onExit} className='col frm-label bg-dark text-light'>
+            <div className='row'>
                 <span className='frm-name'>{framework.name}</span>
                 {framework.image && (
                     <img src={framework.image} alt={framework.name} className='frm-logo' />
                 )}
             </div>
+            {hovering && (
+                <a href={framework.infoLink}>
+                    <div className='row frm-lower'>
+                        <p className='frm-desc'>{framework.desc}</p>
+                    </div>
+                </a>
+            )}
+        </div>
     )
 }
